050f0a933bb5a8d1ea2beb9cc7694ce6f893f7e0
Saienko Yaroslav <masterok92@gmail.com>
2021-06-18 16:24:32 +0300

    Ігнорування спеціальних символів

--- app/src/main/java/com/example/anagram/MainActivity.java
+++ app/src/main/java/com/example/anagram/MainActivity.java
@@ -21,8 +21,13 @@ public class MainActivity extends AppCompatActivity {
     public void onClickConvert(View view)
     {
         String textForConvert = inputText.getText().toString();
-        String reversedString = new StringBuilder(textForConvert).reverse().toString();
-        convertText.setText(reversedString);
+       // String reversedString = new StringBuilder(textForConvert).reverse().toString();
+        //convertText.setText(reversedString);
+        char[] charArray = textForConvert.toCharArray();
+        reverse(charArray);
+        String revStr = new String(charArray);
+        convertText.setText(revStr);
+
     }
 
     private void init()
@@ -30,4 +35,25 @@ public class MainActivity extends AppCompatActivity {
         convertText = findViewById(R.id.tvConvertText);
         inputText= findViewById(R.id.etInputText);
     }
+
+    private void reverse(char str[])
+    {
+        int r = str.length - 1, l = 0;
+        while (l < r)
+        {
+            if (!Character.isAlphabetic(str[l]))
+                l++;
+            else if(!Character.isAlphabetic(str[r]))
+                r--;
+            else
+            {
+                char tmp = str[l];
+                str[l] = str[r];
+                str[r] = tmp;
+                l++;
+                r--;
+            }
+        }
+    }
+
 }
