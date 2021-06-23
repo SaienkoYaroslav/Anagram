d38cbda795d2c32b9221071d0fe39e42763dfc9b
Saienko Yaroslav <masterok92@gmail.com>
2021-06-23 19:57:50 +0300

    final 2.0 with TextWatcher

--- app/src/main/java/com/example/anagram/MainActivity.java
+++ app/src/main/java/com/example/anagram/MainActivity.java
@@ -3,51 +3,60 @@ package com.example.anagram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView convertText;
-    private EditText inputText;
-    private EditText ignoreText;
+    private TextView tv_convertText;
+    private EditText ed_inputText;
+    private EditText ed_ignoreText;
+    private TextView tv_convertTextWatcher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+        ed_inputText.addTextChangedListener(convertWatcher);
+        ed_ignoreText.addTextChangedListener(convertWatcher);
     }
 
-    public void onClickConvert(View view)
-    {
-        String textForConvert = inputText.getText().toString();
-        char[] charArray = textForConvert.toCharArray();
-        reverse(charArray);
-        String revStr = new String(charArray);
-        convertText.setText(revStr);
-
+    public void onClickConvert(View view) {
+        convert(tv_convertText);
     }
 
-    private void init()
-    {
-        convertText = findViewById(R.id.tvConvertText);
-        inputText= findViewById(R.id.etInputText);
-        ignoreText = findViewById(R.id.etIgnore);
+    private final TextWatcher convertWatcher = new TextWatcher() {
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        public void afterTextChanged(Editable s) {
+            convert(tv_convertTextWatcher);
+        }
+    };
+
+    private void init() {
+        tv_convertText = findViewById(R.id.tvConvertText);
+        ed_inputText = findViewById(R.id.etInputText);
+        ed_ignoreText = findViewById(R.id.etIgnore);
+        tv_convertTextWatcher = findViewById(R.id.tv_for_TextWatcher);
     }
 
-    private void reverse(char str[])
-    {
-        String userIgnore = ignoreText.getText().toString();
+    String reverse(String userInput, String userIgnore) {
+        char str[] = userInput.toCharArray();
         int r = str.length - 1, l = 0;
-        while (l < r)
-        {
+        while (l < r) {
             if (userIgnore.indexOf(str[l]) != -1)
                 l++;
-            else if(userIgnore.indexOf(str[r]) != -1)
+            else if (userIgnore.indexOf(str[r]) != -1)
                 r--;
-            else
-            {
+            else {
                 char tmp = str[l];
                 str[l] = str[r];
                 str[r] = tmp;
@@ -55,6 +64,15 @@ public class MainActivity extends AppCompatActivity {
                 r--;
             }
         }
+        String revStr = new String(str);
+        return revStr;
     }
+
+    private void convert(TextView tv) {
+        String textForConvert = ed_inputText.getText().toString();
+        String userIgnore = ed_ignoreText.getText().toString();
+        tv.setText(reverse(textForConvert, userIgnore));
+    }
+
 
 }
