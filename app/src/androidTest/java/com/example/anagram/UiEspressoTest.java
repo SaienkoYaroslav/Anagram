fdf394017747f69744ddd42cb34df38f281bfac0
Saienko Yaroslav <masterok92@gmail.com>
2021-07-09 10:34:50 +0300

    Espresso Testing UI. 9 UI tests 7 successful 2 wrong

--- app/src/androidTest/java/com/example/anagram/UiEspressoTest.java
+++ app/src/androidTest/java/com/example/anagram/UiEspressoTest.java
@@ -3,7 +3,6 @@ package com.example.anagram;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
 
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -23,20 +22,76 @@ public class UiEspressoTest {
     @Rule
     public ActivityTestRule<MainActivity> activityActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
-    @Before
-    public void setUp() throws Exception {
-
-    }
-
     @Test
     public void isDisplayedButtonConvertAfterClick() {
         onView(withId(R.id.buttonConvert)).perform(click()).check(matches(isDisplayed()));
     }
+
     @Test
-    public void testingApp_typeInputAndIgnoreTextPressButtonCheckResult() throws Exception {
+    public void typeInputAndIgnoreTextPressButtonCheckResult() {
         onView(withId(R.id.etInputText)).perform(typeText("TestWord123"));
         onView(withId(R.id.etIgnoreText)).perform(clearText()).perform(typeText("Test123"));
         onView(withId(R.id.buttonConvert)).perform(click());
         onView(withId(R.id.tvConvertText)).check(matches(withText("TestdroW123")));
+    }
+
+    @Test
+    public void pressButtonWhenBothEditTextsEmpty() {
+        onView(withId(R.id.etIgnoreText)).perform(clearText());
+        onView(withId(R.id.buttonConvert)).perform(click());
+        onView(withId(R.id.tvConvertText)).check(matches(withText("")));
+    }
+
+    @Test
+    public void pressButtonWhenEmptyInputTextAndDefaultIgnoreText() {
+        onView(withId(R.id.buttonConvert)).perform(click());
+        onView(withId(R.id.tvConvertText)).check(matches(withText("")));
+
+    }
+
+    @Test
+    public void typeInputTextAndDefaultIgnoreTextPressButtonCheckResult() {
+        onView(withId(R.id.etInputText)).perform(typeText("TestWord123"));
+        onView(withId(R.id.buttonConvert)).perform(click());
+        onView(withId(R.id.tvConvertText)).check(matches(withText("321droWtseT")));
+
+    }
+
+    @Test
+    public void typeInputTextAndClearIgnoreTextPressButtonCheckResult() {
+        onView(withId(R.id.etInputText)).perform(typeText("TestWord123"));
+        onView(withId(R.id.etIgnoreText)).perform(clearText());
+        onView(withId(R.id.buttonConvert)).perform(click());
+        onView(withId(R.id.tvConvertText)).check(matches(withText("321droWtseT")));
+
+    }
+
+    @Test
+    public void typeInputAndIgnoreTextPressButtonCheckResultTwice() {
+        onView(withId(R.id.etInputText)).perform(typeText("TestWord123"));
+        onView(withId(R.id.etIgnoreText)).perform(clearText()).perform(typeText("Test123"));
+        onView(withId(R.id.buttonConvert)).perform(click());
+        onView(withId(R.id.tvConvertText)).check(matches(withText("TestdroW123")));
+        onView(withId(R.id.etInputText)).perform(clearText()).perform(typeText("Apple"));
+        onView(withId(R.id.etIgnoreText)).perform(clearText()).perform(typeText("le"));
+        onView(withId(R.id.buttonConvert)).perform(click());
+        onView(withId(R.id.tvConvertText)).check(matches(withText("ppAle")));
+
+    }
+
+
+    @Test
+    public void typeInputAndIgnoreTextPressButtonCheckResultWrongCase() {
+        onView(withId(R.id.etInputText)).perform(typeText("TestWord123"));
+        onView(withId(R.id.etIgnoreText)).perform(clearText()).perform(typeText("Test123"));
+        onView(withId(R.id.buttonConvert)).perform(click());
+        onView(withId(R.id.tvConvertText)).check(matches(withText("TestdroW12")));
+    }
+
+    @Test
+    public void typeInputTextAndDefaultIgnoreTextWrongCase() {
+        onView(withId(R.id.etInputText)).perform(typeText("TestWord123"));
+        onView(withId(R.id.buttonConvert)).perform(click());
+        onView(withId(R.id.tvConvertText)).check(matches(withText("3e21droWtsT")));
     }
 }
